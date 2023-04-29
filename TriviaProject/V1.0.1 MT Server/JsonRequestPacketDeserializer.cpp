8b7b8803a04a9dd84ff63a239a72009fe7bdb15a
Ori Levi <orilev989@gmail.com>
2023-04-29 16:37:28 +0300

    added deserializeLoginRequest and deserializeSignupRequest

--- TriviaProject/V1.0.1 MT Server/JsonRequestPacketDeserializer.cpp
+++ TriviaProject/V1.0.1 MT Server/JsonRequestPacketDeserializer.cpp
@@ -1,1 +1,21 @@
 #include "JsonRequestPacketDeserializer.h"
+
+LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(std::vector<unsigned char> buffer)
+{
+	nlohmann::json j = nlohmann::json::from_bson(buffer);
+	LoginRequest loginRequest;
+	loginRequest.username = j["username"];
+	loginRequest.password = j["password"];
+	return loginRequest;
+
+}
+
+SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(std::vector<unsigned char> buffer)
+{
+	nlohmann::json j = nlohmann::json::from_bson(buffer);
+	SignupRequest signupRequest;
+	signupRequest.username = j["username"];
+	signupRequest.password = j["password"];
+	signupRequest.email = j["email"];
+	return signupRequest;
+}
