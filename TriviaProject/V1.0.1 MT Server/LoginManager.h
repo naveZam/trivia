257186bc346310aac7249d890c324a878d786207
Ori Levi <orilev989@gmail.com>
2023-04-30 17:41:45 +0300

    added login manager class

---
+++ TriviaProject/V1.0.1 MT Server/LoginManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "LoggedUser.h"
+#include "IDatabase.h"
+#include <vector>
+class LoginManager
+{
+public:
+	void signup(std::string username, std::string password, std::string email);
+	void login(std::string username, std::string password);
+	void logout(std::string username);
+private:
+	IDatabase* m_database;
+	std::vector<LoggedUser> m_loggedUsers;
+};
+
