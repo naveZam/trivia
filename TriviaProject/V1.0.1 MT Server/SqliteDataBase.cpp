430f03a34e21b9fe6ed7a57c5f26b5b0f01eeb41
Ori Levi <orilev989@gmail.com>
2023-04-30 17:26:11 +0300

    added sqliteDatabase and functions to it

---
+++ TriviaProject/V1.0.1 MT Server/SqliteDataBase.cpp
@@ -0,0 +1,61 @@
+#include "SqliteDataBase.h"
+
+bool SqliteDataBase::open()
+{
+	if (sqlite3_open(DATABASE_NAME, &db) == SQLITE_OK)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SqliteDataBase::close()
+{
+	if (sqlite3_close(db) == SQLITE_OK)
+	{
+		return true;
+	}
+	return false;
+}
+
+int SqliteDataBase::doesUserExist(std::string username)
+{
+	std::string sql = "SELECT * FROM users WHERE username = '" + username + "';";
+	sqlite3_stmt* stmt;
+	if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK)
+	{
+		if (sqlite3_step(stmt) == SQLITE_ROW)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int SqliteDataBase::doesPasswordMatch(std::string username, std::string password)
+{
+	std::string sql = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "';";
+	sqlite3_stmt* stmt;
+	if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK)
+	{
+		if (sqlite3_step(stmt) == SQLITE_ROW)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int SqliteDataBase::addNewUser(std::string username, std::string password, std::string email)
+{
+	std::string sql = "INSERT INTO users (username, password, email) VALUES ('" + username + "', '" + password + "', '" + email + "');";
+	sqlite3_stmt* stmt;
+	if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK)
+	{
+		if (sqlite3_step(stmt) == SQLITE_DONE)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
---
+++ TriviaProject/V1.0.1 MT Server/SqliteDataBase.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "IDatabase.h"
+#include "sqlite3.h"
+#define DATABASE_NAME "database.db"
+class SqliteDataBase : public IDatabase
+{
+public:
+	SqliteDataBase();
+	~SqliteDataBase();
+	virtual bool open();
+	virtual bool close();
+	virtual int doesUserExist(std::string username);
+	virtual int doesPasswordMatch(std::string username, std::string password);
+	virtual int addNewUser(std::string username, std::string password, std::string email);
+private:
+	sqlite3* db;
+};
+
