62b704ed19dcdefeda1affdbb6ec5e6e8b2cec76
Nave Zam <ishark584@gmail.com>
2023-05-09 17:20:28 +0300

    Add structs

--- TriviaProject/TriviaProject/JsonResponsePacketSerializer.h
+++ TriviaProject/TriviaProject/JsonResponsePacketSerializer.h
@@ -10,4 +10,12 @@ public:
 	static std::string serializeResponse(ErrorResponse errorResponse);
 	static std::string serializeResponse(LoginResponse loginResponse);
 	static std::string serializeResponse(SignupResponse signupResponse);
+
+	/*static std::string serializeResponse(LogoutResponse);
+	static std::string serializeResponse(GetRoomsResponse);
+	static std::string serializerResponse(GetPlayersInRoomResponse);
+	static std::string serializeResponse(JoinRoomResponse);
+	static std::string serializeResponse(CreateRoomResponse);
+	static std::string serializeResponse(getHighScoreResponse);
+	static std::string serializeResponse(getPersonalStatsResponse);*/
 };
--- TriviaProject/TriviaProject/Structs.h
+++ TriviaProject/TriviaProject/Structs.h
@@ -40,3 +40,60 @@ struct RoomData
 };
 
 typedef unsigned int roomID;
+
+struct LogoutResponse
+{
+	unsigned int status;
+};
+
+struct GetRoomsResponse
+{
+	unsigned int status;
+	std::vector<RoomData> rooms;
+};
+
+struct GetPlayersInRoomResponse
+{
+	unsigned int status;
+	std::vector<std::string> players;
+};
+
+struct getHighScoreResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct getPersonalStatsResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct JoinRoomResponse
+{
+	unsigned int status;
+};
+
+struct CreateRoomResponse
+{
+	unsigned int status;
+};
+
+struct GetPlayersInRoomRequest
+{
+	unsigned int status;
+};
+
+struct JoinRoomRequest
+{
+	unsigned int status;
+};
+
+struct CreateRoomRequest
+{
+	std::string roomName;
+	unsigned int maxUsers;
+	unsigned int questionCount;
+	unsigned int answerTimeout;
+};
