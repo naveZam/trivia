d7591d834df17e1f7f6eaa491ab75a1f5619119f
Ori Levi <orilev989@gmail.com>
2023-06-05 12:32:40 +0300

    added leaveroom and getroomstate to roommemberrequesthandler

--- TriviaProject/TriviaProject/RoomMemberRequestHandler.cpp
+++ TriviaProject/TriviaProject/RoomMemberRequestHandler.cpp
@@ -36,3 +36,49 @@ RequestResult RoomMemberRequestHandler::handleRequest(RequestInfo info)
 	result.response = std::vector<unsigned char>(Respones.begin(), Respones.end());
 	return result;
 }
+
+RequestResult RoomMemberRequestHandler::leaveRoom(RequestInfo info)
+{
+	RequestResult result;
+	result.newHandler = this;
+	std::string notErrorrRespond = "1";
+	std::vector<unsigned char> nonError = std::vector<unsigned char>(notErrorrRespond.begin(), notErrorrRespond.end());
+	if (info.id == LeaveRoomRequest)
+	{
+		m_room.removeUser(M_user);
+		result.response = nonError;
+		return result;
+	}
+	else
+	{
+		ErrorResponse ErrorRes;
+		std::string Respones = JsonResponsePacketSerializer::serializeResponse(ErrorRes);
+		result.response = std::vector<unsigned char>(Respones.begin(), Respones.end());
+		return result;
+	}
+	
+}
+
+RequestResult RoomMemberRequestHandler::getRoomState(RequestInfo info)
+{
+	RequestResult result;
+	result.newHandler = this;
+	std::string notErrorrRespond = "1";
+	std::vector<unsigned char> nonError = std::vector<unsigned char>(notErrorrRespond.begin(), notErrorrRespond.end());
+	if (info.id == GetRoomStateRequest)
+	{
+		GetRoomStateResponse getRoomStateRes;
+		getRoomStateRes.status = m_room.getRoomData().isActive;
+		getRoomStateRes.players = m_room.getAllUsers();
+		std::string Respones = JsonResponsePacketSerializer::serializeResponse(getRoomStateRes);
+		result.response = std::vector<unsigned char>(Respones.begin(), Respones.end());
+		return result;
+	}
+	else
+	{
+		ErrorResponse ErrorRes;
+		std::string Respones = JsonResponsePacketSerializer::serializeResponse(ErrorRes);
+		result.response = std::vector<unsigned char>(Respones.begin(), Respones.end());
+		return result;
+	}
+}
